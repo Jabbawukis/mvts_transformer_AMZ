1ae89b7c952dc17c54d2914c73601be42b695408
daniel.christoph <daniel.christoph@open5g-test.com>
2024-01-14 22:13:11 +0100

    add experiments

--- run_train.sh
+++ run_train.sh
@@ -13,6 +13,8 @@
 #--task classification \
 #--key_metric accuracy
 
+# --output_dir ../experiments --comment "classification from Scratch" --name Sp_Arab_Digits_fromScratch --records_file Classification_records.xls --data_dir ../Datasets/Classification/SpokenArabicDigits/ --data_class tsra --pattern TRAIN --val_pattern TEST --epochs 400 --lr 0.001 --optimizer RAdam --pos_encoding learnable --task classification --key_metric accuracy
+
 python3 src/main.py --output_dir experiments \
 --comment "classification from Scratch" \
 --name Sp_Arab_Digits_fromScratch \
