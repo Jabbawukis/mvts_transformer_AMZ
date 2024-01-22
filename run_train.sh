46b8b1ae5c32d54ca290badfc5fbf9fe69b89329
daniel.christoph <daniel.christoph@open5g-test.com>
2024-01-22 21:53:33 +0100

    refactor

--- run_train.sh
+++ run_train.sh
@@ -1,31 +1,76 @@
-#python3 src/main.py --output_dir experiments \
-#--comment "classification from Scratch" \
-#--name Abnormal_fromScratch \
-#--records_file Classification_records.xls \
-#--data_dir Datasets/Classification/AbnormalHeartbeat/ \
-#--data_class tsra \
-#--pattern TRAIN \
-#--val_pattern TEST \
-#--epochs 400 \
-#--lr 0.001 \
-#--optimizer RAdam  \
-#--pos_encoding learnable  \
-#--task classification \
-#--key_metric accuracy
+python src/main.py --output_dir experiments \
+--comment "pretraining classification" \
+--name SelfRegulationSCP2_pretraining \
+--data_dir /vol/fob-vol4/mi17/christod/Zeitreihenanalyse/mvts_transformer_AMZ/Datasets/Classification/SelfRegulationSCP2 \
+--data_class tsra \
+--pattern TRAIN \
+--val_ratio 0.2 \
+--epochs 700 \
+--lr 0.001 \
+--optimizer RAdam \
+--batch_size 128 \
+--pos_encoding learnable \
+--d_model 256 \
+--dim_feedforward 512 \
+--num_heads 8 \
+--num_layers 3
 
-# --output_dir ../experiments --comment "classification from Scratch" --name Sp_Arab_Digits_fromScratch --records_file Classification_records.xls --data_dir ../Datasets/Classification/SpokenArabicDigits/ --data_class tsra --pattern TRAIN --val_pattern TEST --epochs 400 --lr 0.001 --optimizer RAdam --pos_encoding learnable --task classification --key_metric accuracy
-
-python3 src/main.py --output_dir experiments \
---comment "classification from Scratch" \
---name Sp_Arab_Digits_fromScratch \
---records_file Classification_records.xls \
---data_dir Datasets/Classification/SpokenArabicDigits/ \
+python src/main.py --output_dir experiments \
+--comment "finetune for classification" \
+--name SelfRegulationSCP2_finetuned \
+--data_dir /vol/fob-vol4/mi17/christod/Zeitreihenanalyse/mvts_transformer_AMZ/Datasets/Classification/SelfRegulationSCP2  \
 --data_class tsra \
 --pattern TRAIN \
 --val_pattern TEST \
---epochs 400 \
+--epochs 100 \
 --lr 0.001 \
 --optimizer RAdam  \
---pos_encoding learnable  \
+--batch_size 128 \
+--pos_encoding learnable \
+--load_model model_best.pth \
 --task classification \
---key_metric accuracy
+--change_output \
+--key_metric accuracy \
+--d_model 256 \
+--dim_feedforward 512 \
+--num_heads 8 \
+--num_layers 3
+#################################################################################
+
+python src/main.py --output_dir experiments \
+--comment "pretraining classification" \
+--name JapaneseVowels_pretraining \
+--data_dir /vol/fob-vol4/mi17/christod/Zeitreihenanalyse/mvts_transformer_AMZ/Datasets/Classification/JapaneseVowels \
+--data_class tsra \
+--pattern TRAIN \
+--val_ratio 0.2 \
+--epochs 700 \
+--lr 0.001 \
+--optimizer RAdam \
+--batch_size 128 \
+--pos_encoding learnable \
+--d_model 128 \
+--dim_feedforward 256 \
+--num_heads 8 \
+--num_layers 3
+
+python src/main.py --output_dir experiments \
+--comment "finetune for classification" \
+--name JapaneseVowels_finetuned \
+--data_dir  \
+--data_class tsra \
+--pattern TRAIN \
+--val_pattern TEST \
+--epochs 100 \
+--lr 0.001 \
+--optimizer RAdam  \
+--batch_size 128 \
+--pos_encoding learnable \
+--load_model model_best.pth \
+--task classification \
+--change_output \
+--key_metric accuracy \
+--d_model 128 \
+--dim_feedforward 256 \
+--num_heads 8 \
+--num_layers 3
